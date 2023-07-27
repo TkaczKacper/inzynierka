5c37e749aac32ac69f38365412901963c13d53b6
TkaczKacper <kacpertk2016@gmail.com>
2023-07-27 15:58:04 +0200

    [Server] auth controller

--- server/server/Controllers/AuthController.cs
+++ server/server/Controllers/AuthController.cs
@@ -2,9 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using server.ApiResponses;
-using server.Data;
+using server.Helpers;
 using server.Models;
+using server.Models.Authenticate;
+using server.Services;
 using server.Utilities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,124 +13,56 @@ using System.Text;
 
 namespace server.Controllers
 {
-    [Route("api/auth/")]
+    [Authorize]
     [ApiController]
+    [Route("api/auth/")]
     public class AuthController : ControllerBase
     {
-        private readonly DataContext context;
-        private IConfiguration _configuration;
+        private IUserService _userService;
 
-        public AuthController(
-            IConfiguration configuration, 
-            DataContext _context)
+        public AuthController(IUserService userService)
         {
-            _configuration = configuration;
-            context = _context;
+            _userService = userService;
         }
 
-        [Authorize]
         [HttpGet("/secret")]
         public IActionResult Secret()
         {
-            LoginResponse response = new() { };
+            return new JsonResult(new { message = "Hello there." }) { StatusCode = StatusCodes.Status200OK };
+        }
 
-            response.type = "XD";
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(
+            [FromBody] AuthRequest userLogin, CancellationToken cancellationToken)
+        {
+            var response = _userService.Authenticate(userLogin, ipAddress());
+            setTokenCookie(response.RefreshToken);
+
             return Ok(response);
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(
-            [FromBody] UserLogin userLogin, CancellationToken cancellationToken)
+
+
+        // helper methods
+        private void setTokenCookie(string token)
         {
-            User? user = await Authenticate(userLogin);
-            LoginResponse response = new() { };
-
-            if (user is not null)
+            // append cookie with refresh token to the http response
+            var cookieOptions = new CookieOptions
             {
-                response.type = "success";
-                string token = GenerateToken(user);
-                response.error = token;
-                Console.WriteLine(token);
-
-                return Ok(response);
-            }
-
-            response.type = "error";
-            response.error = "Invalid credentials.";
-            return NotFound(response);
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(
-            [FromBody] UserRegister request, CancellationToken cancellationToken
-        )
-        {
-            User? newUser = await this.context.Users.Where(u => u.Username == request.Username || u.Email == request.Email).FirstOrDefaultAsync();
-            LoginResponse response = new() {};
-            Console.WriteLine(request.Email + request.Password + request.Username);
-
-            if (newUser is null)
-            {
-                var password = PasswordHasher.Hash(request.Password).Result;
-
-                var registerUser = new User()
-                {
-                    Username = request.Username,
-                    Password = password,
-                    Email = request.Email,
-                    RegisterDate = DateTime.UtcNow                    
-                };
-
-                context.Users.Add(registerUser);
-                context.SaveChanges();
-
-                response.type = "success";
-
-                return Ok(response);
-            }
-            
-            response.type = "error";
-            response.error = "Username or/and email taken.";
-            return BadRequest(response);
-        }
-
-        private async Task<User?> Authenticate(UserLogin userLogin)
-        {
-            User? possibleUser = await this.context.Users.Where(u => u.Username == userLogin.Username).FirstOrDefaultAsync();
-
-            if (possibleUser != null && PasswordHasher.Verify(possibleUser.Password, userLogin.Password).Result)
-            {
-                return possibleUser;
-            }
-
-            return null;
-        }
-
-        private string GenerateToken(User user)
-        {
-            var claims = new Claim[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, user.ID.ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+                HttpOnly = true,
+                Expires = DateTime.UtcNow.AddDays(31)
             };
+            Response.Cookies.Append("refreshToken", token, cookieOptions);
+        }
 
-            var signingCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(_configuration["JWT:Key"])),
-                SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-               "Issuer",
-               "Audience",
-               claims,
-               null,
-               DateTime.UtcNow.AddDays(31),
-               signingCredentials);
-
-            string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return tokenValue;
+        private string ipAddress()
+        {
+            // get source ip address for request
+            if (Request.Headers.ContainsKey("X-Forwarded-For"))
+                return Request.Headers["X-Forwarded_For"];
+            else
+                return HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
         }
     }
 }
