839e9a86d892f7d2ee9b8be32ae115a636147087
TkaczKacper <kacpertk2016@gmail.com>
2023-07-27 13:55:22 +0200

    [server] error handler

---
+++ server/server/Helpers/ErrorHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace server.Helpers
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _request;
+
+        public ErrorHandlerMiddleware(RequestDelegate request)
+        {
+            _request = request;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _request(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case AppException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
