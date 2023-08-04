8feba50e2407fe15b0a3ec8525ec42b5ebcc25a5
TkaczKacper <kacpertk2016@gmail.com>
2023-08-04 13:50:19 +0200

    [client] login form validation with yup&formik

--- client/app/login/loginForm.tsx
+++ client/app/login/loginForm.tsx
@@ -1,36 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-const LoginForm = () => {
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
-   console.log(username, password);
+interface FormValues {
+   username: string;
+   password: string;
+}
 
-   const loginHandler = () => {};
+const passwordRule =
+   /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!-\/:-@[-\`]).{8,32}$/;
+const LoginSchema = Yup.object().shape({
+   username: Yup.string()
+      .min(6, "Username too short.")
+      .max(32, "Username too long.")
+      .required("Field required."),
+   password: Yup.string()
+      .min(8, "Password too short.")
+      .max(32, "Password too long.")
+      .matches(passwordRule, "Weak Password.")
+      .required("Field required."),
+});
+
+export const LoginForm: React.FC<{}> = () => {
+   const initialValues: FormValues = { username: "", password: "" };
 
    return (
-      <form onSubmit={loginHandler}>
-         <label htmlFor="username">Username: </label>
-         <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-         />
-
-         <label htmlFor="password">Password: </label>
-         <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-         />
-         <button>Login</button>
-      </form>
+      <Formik
+         initialValues={initialValues}
+         validationSchema={LoginSchema}
+         onSubmit={(values, actions) => {
+            console.log(values);
+            actions.setSubmitting(false);
+         }}
+      >
+         {({ errors, touched }: any) => (
+            <Form>
+               <Field name="username" />
+               {errors.username && touched.username ? (
+                  <div>{errors.username}</div>
+               ) : null}
+               <Field name="password" />
+               {errors.password && touched.password ? (
+                  <div>{errors.password}</div>
+               ) : null}
+               <button type="submit">Login</button>
+            </Form>
+         )}
+      </Formik>
    );
 };
-
-export default LoginForm;
