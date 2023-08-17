53ec8f8003f145a636f15a6c168886ebfbfcc0c5
TkaczKacper <kacpertk2016@gmail.com>
2023-08-17 15:17:20 +0200

    register

---
+++ server/server/Models/Authenticate/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Authenticate
+{
+    public class LoginRequest
+    {
+        [StringLength(32, MinimumLength = 6, ErrorMessage = "Username too short.")]
+        [Required(ErrorMessage = "Username required.")]
+        public string Username { get; set; } = null!;
+
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!-\/:-@[-\`]).{8,32}$", ErrorMessage = "Password too weak.")]
+        [Required(ErrorMessage = "Password required.")]
+        public string Password { get; set; } = null!;
+
+    }
+}
--- server/server/Models/Authenticate/RegisterRequest.cs
+++ server/server/Models/Authenticate/RegisterRequest.cs
@@ -1,16 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Models.Authenticate
 {
-    public class AuthRequest
+    public class RegisterRequest
     {
         [StringLength(32, MinimumLength = 6, ErrorMessage = "Username too short.")]
         [Required(ErrorMessage = "Username required.")]
         public string Username { get; set; } = null!;
 
+        [Required(ErrorMessage = "Email required.")]
+        [EmailAddress(ErrorMessage = "Email incorrect.")]
+        public string Email { get; set; } = null!;
+
         [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!-\/:-@[-\`]).{8,32}$", ErrorMessage = "Password too weak.")]
         [Required(ErrorMessage = "Password required.")]
         public string Password { get; set; } = null!;
 
+        [NotMapped]
+        [Required(ErrorMessage = "Password required.")]
+        [Compare(nameof(Password), ErrorMessage = "Password doesn't match.")]
+        public string RepeatPassword { get; set; } = null!;
     }
 }
