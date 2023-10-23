0c1de7272879c42ed650c038a46718e43aa0f5db
TkaczKacper <kacpertk2016@gmail.com>
2023-10-23 19:46:50 +0200

    page with loading

--- client/app/activities/[id]/page.tsx
+++ client/app/activities/[id]/page.tsx
@@ -3,23 +3,42 @@
 import { useEffect, useState } from "react";
 import { getActivityDataById } from "@/utils/serverUtils";
 import { Activity } from "@/app/profile/[id]/types";
+import Loading from "@/app/loading";
+import Link from "next/link";
+import { useUserContext } from "@/contexts/UserContextProvider";
 
 const page = () => {
   const [activity, setActivity] = useState<Activity>();
+  const [loading, setLoading] = useState(true);
+  const { userId } = useUserContext();
 
   useEffect(() => {
     const activityId = Number(window.location.pathname.split("/")[2]);
     const getActivityData = async (activityId: number) => {
       const res = await getActivityDataById(activityId);
+      setLoading(false);
       setActivity(res?.data);
     };
     getActivityData(activityId);
   }, []);
   console.log(activity);
   return (
-    <div>
-      <h1>activity page</h1>
-    </div>
+    <>
+      {!loading ? (
+        <div>
+          {activity ? (
+            <h1>activity page</h1>
+          ) : (
+            <>
+              <h1>Activity not found.</h1>
+              <Link href={`/profile/${userId}`}>Back to the profile page.</Link>
+            </>
+          )}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
