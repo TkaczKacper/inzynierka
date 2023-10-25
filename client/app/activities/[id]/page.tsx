b85e2aa6ac1326b2303fcb2e9ad476287129e24b
TkaczKacper <kacpertk2016@gmail.com>
2023-10-25 14:41:40 +0200

    map added

--- client/app/activities/[id]/page.tsx
+++ client/app/activities/[id]/page.tsx
@@ -6,6 +6,8 @@ import { Activity } from "@/app/profile/[id]/types";
 import Loading from "@/app/loading";
 import Link from "next/link";
 import { useUserContext } from "@/contexts/UserContextProvider";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const page = () => {
   const [activity, setActivity] = useState<Activity>();
@@ -27,7 +29,29 @@ const page = () => {
       {!loading ? (
         <div>
           {activity ? (
-            <h1>activity page</h1>
+            <>
+              <h1>activity page</h1>
+              <div style={{ height: 593, width: 890 }}>
+                <MapContainer
+                  center={[activity.startLatLng[0], activity.startLatLng[1]]}
+                  zoom={13}
+                  scrollWheelZoom={false}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker
+                    position={[
+                      activity.startLatLng[0],
+                      activity.startLatLng[1],
+                    ]}
+                  >
+                    <Popup>hi</Popup>
+                  </Marker>
+                </MapContainer>
+              </div>
+            </>
           ) : (
             <>
               <h1>Activity not found.</h1>
