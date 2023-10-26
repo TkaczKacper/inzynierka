501dff3b14e1d002ed9576c7e0c9d77721cadd0b
TkaczKacper <kacpertk2016@gmail.com>
2023-10-26 18:26:18 +0200

    chart interval update

--- client/charts/ChartController.tsx
+++ client/charts/ChartController.tsx
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from "react";
 // @ts-ignore
 import CanvasJSReact from "@canvasjs/react-charts";
+import { parseDurationNumeric } from "@/utils/parseDuration";
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -13,28 +14,77 @@ class ChartController extends Component<ChartProps> {
   render() {
     const dataPoints: { x: number; y: number }[] = [];
     this.props.data.map((value, index) => {
-      return dataPoints.push({ x: index, y: value });
+      return dataPoints.push({ x: index + 1, y: value });
     });
     console.log(this.props.data);
     const options = {
+      scales: {},
       title: {
         text: "Power curve",
+      },
+      axisX: {
+        labelAutoFit: false,
+        crosshair: {
+          enabled: true, //disable here
+        },
+        stripLines: [
+          {
+            value: 2,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 5,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 10,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 30,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 60,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 180,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 300,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 600,
+            color: "black",
+            thickness: 1,
+          },
+          {
+            value: 1200,
+            color: "black",
+            thickness: 1,
+          },
+        ],
+        logarithmic: true,
+        // interval: 0.2,
+        interval: Math.log10(1.6),
+        labelFormatter: function (e: any) {
+          return parseDurationNumeric(e.value);
+        },
       },
       data: [
         {
           type: "line",
-          toolTipContent: "Watts: {y}",
-          dataPoints: [
-            this.props.data.map((value, index) => {
-              return { x: index, y: value };
-            }),
-          ],
-        },
-      ],
-      data: [
-        {
-          type: "line",
-          toolTipContent: "Week {x}: {y}%",
+          toolTipContent: "Time: {x}s, Watts: {y}",
           dataPoints: dataPoints,
         },
       ],
